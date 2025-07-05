4176dbf3877c22bba813b23fb7dcfeb4a2f1b770
Ukaegbu Gray Nneji <graynneji@outlook.com>
2025-07-05 07:29:49 -0700

    reservation

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -47,6 +47,7 @@ function ReservationForm({ cabin, user }) {
           resetRange();
         }}
         // action={createBookingWithData}
+        // action={createBooking}
         className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col"
       >
         <div className="space-y-2">
